f337ec1b41a090ca50db318b14de695aa605a327
Tzanguyn <tzanguytzanguyeen@gmail.com>
2025-09-26 23:21:46 +0700

    chore: remove .first-commit markers

--- app.js
+++ app.js
@@ -92,4 +92,3 @@ app.use(express.static("public"));
 app.listen(PORT, () => {
   console.log(`Server is running on port http://localhost:${PORT}`);
 });
-  // first commit marker for GitHub folder summary lines
